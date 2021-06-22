0231bde2b28854f19f9cd8117859cce5ce7643a1
Joel Rojas <jr.bestone17@gmail.com>
2021-06-22 10:42:59 -0400

    Add class to read properties file

--- src/main/java/endpointurl/Endpoint.java
+++ src/main/java/endpointurl/Endpoint.java
@@ -2,5 +2,5 @@ package endpointurl;
 
 public class Endpoint {
     public static final String PEOPLE = "/" + Element.INDIVIDUAL;
-    public static final String PERSON = PEOPLE + "/" + ElementParam.ID;
+    public static final String PERSON = PEOPLE + "/{" + ElementParam.ID + "}";
 }
